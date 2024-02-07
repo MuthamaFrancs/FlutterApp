928b27be8c0699e56512add6c7f1e71f17de8d90
MuthamaFrancis <francisfrancs02@gmail.com>
2024-02-07 11:49:57 +0300

    obscuretext,

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/configs/constants.dart';
 import 'package:flutter_application_1/views/CustomButton.dart';
@@ -22,41 +23,63 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Login to My app")),
+        title: const Center(child: Text("Login to myapp")),
         backgroundColor: primaryColor,
         foregroundColor: whitecolor,
       ),
       //this is the body
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: customText(
+      //SINGLE CHILD SCROLL VIEW WIDGET
+      body: SingleChildScrollView(
+        //PADDING WIDGET
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset("assets/images/myapplogo.jpg",width: 80,height: 80, ),
+              ),
+              customText(label: "Login", style: const TextStyle( fontWeight: FontWeight.w700)),
+              customText(
               label: "Username",
-              style: const TextStyle(fontWeight: FontWeight.w200),
-            ),
+                  style: const TextStyle(fontWeight: FontWeight.w200),
+              ),
+              //usename controller
+              customTextField(
+              labelText: "Username", 
+              controller: usernamecontroller,hintText: "example@gmail.com, phone no",icon: Icons.person,
+              ),
+              //SizedBox Widget 
+              const SizedBox(height: 10,),
+              //customText Widget for password
+              customText(
+                  label: "Password",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w200,
+                  )
+                  ),
+              //password controller = WITH OBSCUREED TEXT
+              customTextField(labelText: "Password", controller: passwordcontroller,hintText: "password",icon: Icons.lock,obscureText: true, Iconsuffix: Icons.visibility_off,),
+              //customText(label: "ForgotPassword", style: FontWeight: fontWeight.),
+              //ELevated Buton
+              customButton(
+                  btnLabel: "Proceed",
+                  btnColor: primaryColor,
+                  onPressed: () {
+                    // Handle button press
+                    print("Button clicked ${usernamecontroller.text}");
+                  },
+                  style: const ButtonStyle(
+                    alignment: Alignment.center,
+                  ))
+                //sized box widget
+                
+            ],
           ),
-          customTextField(
-          labelText: "Username", 
-          controller: usernamecontroller
-          ),
-          customText(
-              label: "Password",
-              style: const TextStyle(
-                fontWeight: FontWeight.w200,
-              )),
-          customTextField(labelText: "Password", controller: passwordcontroller),
-          customButton(
-              btnLabel: "Proceed",
-              btnColor: primaryColor,
-              onPressed: () {
-                // Handle button press
-              },
-              style: const ButtonStyle(
-                alignment: Alignment.center,
-              ))
-        ],
+        ),
       ),
     );
   }
